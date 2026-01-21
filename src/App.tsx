4d2d53aa845aa57d4938811e8238bed77214c44c
Royce Joe <roylory@gmail.com>
2026-01-21 11:59:43 -0800

    local city as the first city

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import { ScrollableReel } from "./components/ScrollableReel";
+import type { City } from "./types/city";
 
-const cities = [
+const baseCities: City[] = [
   { id: "la", label: "LA", tz: "America/Los_Angeles" },
   { id: "nyc", label: "NYC", tz: "America/New_York" },
   { id: "lon", label: "London", tz: "Europe/London" },
-  // { id: "par", label: "Paris", tz: "Europe/Paris" },
   { id: "sel", label: "Seoul", tz: "Asia/Seoul" },
 ];
+
+const tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
+const localCity: City = {
+  id: "local",
+  label: "Local",
+  tz,
+}
+
+const cities = [localCity, ...baseCities.filter((city) => city.tz !== tz)];
 
 export default function App() {
   return (
