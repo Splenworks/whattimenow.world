2746ea170b81e145e8ed8f40fcea61383f3a0d36
Royce Joe <roylory@gmail.com>
2026-01-28 06:36:37 -0800

    about page
    chatgpt generated

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import { HomePage } from "./pages/Home"
 import { PrivacyPage } from "./pages/Privacy"
 import { TermsPage } from "./pages/Terms"
+import { AboutPage } from "./pages/About"
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="/privacy" element={<PrivacyPage />} />
       <Route path="/terms" element={<TermsPage />} />
+      <Route path="/about" element={<AboutPage />} />
     </Routes>
   )
 }
