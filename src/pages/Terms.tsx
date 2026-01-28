e488dd159672563bccd06c9162fe8cdb45d4b84d
Royce Joe <roylory@gmail.com>
2026-01-28 15:22:01 -0800

    scroll to top

--- src/pages/Terms.tsx
+++ src/pages/Terms.tsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom"
+import { useScrollToTop } from "../hooks/useScrollToTop"
 
 export function TermsPage() {
+  useScrollToTop()
+
   return (
     <main className="min-h-screen bg-white text-gray-900 dark:bg-gray-950 dark:text-gray-100">
       <div className="mx-auto w-full max-w-180 px-4 py-10">
