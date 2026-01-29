fda33078d6c6fd09083d553a99e5c20be7739f4b
Royce Joe <roylory@gmail.com>
2026-01-28 22:33:16 -0800

    footer header margin and border

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,21 +2,28 @@ import { Link } from "react-router-dom"
 
 export function Footer() {
   return (
-    <footer>
-      <div className="mx-auto w-full pt-6 pb-6 space-y-3 text-center text-sm text-gray-500 dark:text-gray-400">
+    <footer className="mt-4 border-t border-gray-100 dark:border-gray-800">
+      <div className="mx-auto w-full space-y-3 p-6 text-center text-sm text-gray-500 dark:text-gray-400">
+        <p>&copy; {new Date().getFullYear()} Splenworks Inc.</p>
         <p>
-          &copy; {new Date().getFullYear()} Splenworks Inc.
-        </p>
-        <p>
-          <Link to="/about" className="hover:underline hover:text-gray-700 dark:hover:text-gray-300">
+          <Link
+            to="/about"
+            className="hover:text-gray-700 hover:underline dark:hover:text-gray-300"
+          >
             About
           </Link>
           <span className="mx-2 text-gray-400 dark:text-gray-600">•</span>
-          <Link to="/privacy" className="hover:underline hover:text-gray-700 dark:hover:text-gray-300">
+          <Link
+            to="/privacy"
+            className="hover:text-gray-700 hover:underline dark:hover:text-gray-300"
+          >
             Privacy
           </Link>
           <span className="mx-2 text-gray-400 dark:text-gray-600">•</span>
-          <Link to="/terms" className="hover:underline hover:text-gray-700 dark:hover:text-gray-300">
+          <Link
+            to="/terms"
+            className="hover:text-gray-700 hover:underline dark:hover:text-gray-300"
+          >
             Terms
           </Link>
           {/* <span className="mx-2 text-gray-400 dark:text-gray-600">•</span>
