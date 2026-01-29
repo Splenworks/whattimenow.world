d6e3f37247f131cd52ee270eff28294c67c2ecc6
Royce Joe <roylory@gmail.com>
2026-01-29 14:47:47 -0800

    refactor: center on "now" when route changes

--- src/pages/WorldTime.tsx
+++ src/pages/WorldTime.tsx
@@ -3,6 +3,9 @@ import { WorldTimeApp } from "../components/WorldTimeApp"
 
 export function WorldTimePage() {
   const { cityIds } = useCityParams()
+  const routedCityIds = ["local", ...cityIds, "utc"]
+  const routeKey = routedCityIds.join("|")
 
-  return <WorldTimeApp cityIds={["local", ...cityIds, "utc"]} lockCities />
+  // Force remount on route change so TimeReel re-centers on "Now".
+  return <WorldTimeApp key={routeKey} cityIds={routedCityIds} lockCities />
 }
